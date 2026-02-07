2dcf57d17baaa2ea0325fa3b4ad7b1f7036941be
Andrey Tretyakov <813860+a3kov@users.noreply.github.com>
2026-02-07 16:21:35 -0300

    Prepare version 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v0.1.1 (2026-02-07)
+
+Minor improvements and docs updates
+
 ## v0.1.0 (2026-02-06)
 
-- Initial Release
+Initial Release
--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule AtomicBucket.MixProject do
   def project do
     [
       app: :atomic_bucket,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
