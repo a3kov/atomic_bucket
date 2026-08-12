6387e0a90eac0e1bd9dfe2017d5530b422579f07
Andrey Tretyakov <813860+a3kov@users.noreply.github.com>
2026-08-11 22:47:07 -0300

    Separate request and raw_request in the benchmark and make it look better.

--- bench/benchmark.exs
+++ bench/benchmark.exs
@@ -11,64 +11,89 @@ burst = 1_000
 capacity = 1_000
 refill_ms = 5
 cost = 1
-parallel = String.to_integer(System.get_env("PARALLEL", "1"))
+iter_requests = 1_000
 
 AtomicBucket.start_link([])
 
+IO.puts(
+  """
+  ###############################################################################################################
+  #                                                R E Q U E S T                                                #
+  ###############################################################################################################
+  """
+)
 Benchee.run(
   %{
-    "request (non-literals, default opts)" => {
-      fn key ->
-        for _ <- 1..1000 do
-          AtomicBucket.request(key, window, requests, burst)
-        end
-      end,
-      before_scenario: fn _ -> :erlang.unique_integer([:positive]) end
-    },
-    "request (literals, default opts)" => {
-      fn key ->
-        for _ <- 1..1000 do
-          AtomicBucket.request(key, 1, 5_000, 1_000)
+    "request (literals, reusing ref)" => {
+      fn bucket ->
+        {_, _, ref} = AtomicBucket.request(bucket, 1, 5_000, 1_000)
+
+        for _ <- 1..(iter_requests - 1) do
+          AtomicBucket.request(bucket, 1, 5_000, 1_000, ref: ref)
         end
       end,
       before_scenario: fn _ -> :erlang.unique_integer([:positive]) end
     },
     "request (literals, persistent)" => {
-      fn key ->
-        for _ <- 1..1000 do
-          AtomicBucket.request(key, 1, 5_000, 1_000, persistent: true)
+      fn bucket ->
+        for _ <- 1..iter_requests do
+          AtomicBucket.request(bucket, 1, 5_000, 1_000, persistent: true)
         end
       end,
       before_scenario: fn _ -> :erlang.unique_integer([:positive]) end
     },
-    "request (literals, reusing ref)" => {
-      fn key ->
-        {_, _, ref} = AtomicBucket.request(key, window, requests, burst)
-
-        for _ <- 1..999 do
-          AtomicBucket.request(key, 1, 5_000, 1_000, ref: ref)
+    "request (literals, default opts)" => {
+      fn bucket ->
+        for _ <- 1..iter_requests do
+          AtomicBucket.request(bucket, 1, 5_000, 1_000)
         end
       end,
       before_scenario: fn _ -> :erlang.unique_integer([:positive]) end
     },
-    "raw_request (non-literals, default opts)" => {
-      fn key ->
-        for _ <- 1..1000 do
-          AtomicBucket.raw_request(key, capacity, refill_ms, cost)
-        end
-      end,
-      before_scenario: fn _ -> :erlang.unique_integer([:positive]) end
-    },
-    "raw_request (literals, default opts)" => {
-      fn key ->
-        for _ <- 1..1000 do
-          AtomicBucket.raw_request(key, 1_000, 5, 1)
+    "request (non-literals, default opts)" => {
+      fn bucket ->
+        for _ <- 1..iter_requests do
+          AtomicBucket.request(bucket, window, requests, burst)
         end
       end,
       before_scenario: fn _ -> :erlang.unique_integer([:positive]) end
     },
   },
+  exclude_outliers: true,
   formatters: [{Benchee.Formatters.Console, extended_statistics: true}],
-  time: 5,
-  parallel: parallel
+  print: [configuration: false],
+  time: 5
+)
+
+IO.puts(
+  """
+
+  ###############################################################################################################
+  #                                              R A W  R E Q U E S T                                           #
+  ###############################################################################################################
+  """
+)
+Benchee.run(
+  %{
+    "raw_request (literals, default opts)" => {
+      fn bucket ->
+        for _ <- 1..iter_requests do
+          AtomicBucket.raw_request(bucket, 1_000, 5, 1)
+        end
+      end,
+      before_scenario: fn _ -> :erlang.unique_integer([:positive]) end
+    },
+    "raw_request (non-literals, default opts)" => {
+      fn bucket ->
+        for _ <- 1..iter_requests do
+          AtomicBucket.raw_request(bucket, capacity, refill_ms, cost)
+        end
+      end,
+      before_scenario: fn _ -> :erlang.unique_integer([:positive]) end
+    },
+  },
+  exclude_outliers: true,
+  formatters: [{Benchee.Formatters.Console, extended_statistics: true}],
+  print: [configuration: false],
+  time: 5
 )
