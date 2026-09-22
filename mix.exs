25f69e5ac26c2cd6016175f787910fef6af27e86
Andrey Tretyakov <813860+a3kov@users.noreply.github.com>
2026-09-21 23:40:20 -0300

    Formatting.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule AtomicBucket.MixProject do
       app: :atomic_bucket,
       version: "0.4.1",
       elixir: "~> 1.17",
-      elixirc_paths: elixirc_paths(Mix.env),
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       source_url: @source_url,
